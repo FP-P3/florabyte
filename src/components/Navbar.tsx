4e16359bdb945fbce43b8473212ef6a91b911753
fathirachmann <fathirachmann@gmail.com>
2025-09-19 22:24:11 +0700

    fix: update logout redirection to login page and clean up Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,6 @@ export default async function Navbar() {
   const cookieStore = await cookies();
   const auth = cookieStore.get("Authorization");
   const isSignedIn = Boolean(auth?.value);
-  // const isSignedIn = true; // contoh
 
   return <NavbarClient isSignedIn={isSignedIn} />;
 }
