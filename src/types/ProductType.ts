ba0b8d3a3fe332f614ef4c49b5a516b1de050fd3
farhanrandy <farhanmohy@gmail.com>
2025-09-21 21:39:40 +0700

    fix: correct ProductType definition by ensuring _id is required and fixing formatting

--- src/types/ProductType.ts
+++ src/types/ProductType.ts
@@ -1,14 +1,14 @@
-import { ObjectId } from "mongodb"
+import { ObjectId } from "mongodb";
 
 export type ProductType = {
-  _id?: ObjectId
-  name: string
-  description: string
-  price: number
-  stock: number
-  imgUrl: string
-  category: string
-  embedding?: number[]
-  createdAt?: Date
-  updatedAt?: Date
-}
+  _id: string | ObjectId;
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  imgUrl: string;
+  category: string;
+  embedding?: number[];
+  createdAt?: Date;
+  updatedAt?: Date;
+};
